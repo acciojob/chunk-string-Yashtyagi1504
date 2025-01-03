1ffde24b30001279e613be3573b55f5b3c111734
Yashtyagi1504 <166196060+Yashtyagi1504@users.noreply.github.com>
2025-01-04 03:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function stringChop(str, size) {
-  // your code here
+  let arr = []
+    for(let i = 0;i<str.length;i+=size){
+        let str1 = ""
+        for(let j = i;j<i+size && j<str.length;j++){
+            str1 += str[j]
+        }
+        arr.push(str1)
+    }
+    return arr
 }
 
 // Do not change the code below
 const str = prompt("Enter String.");
 const size = prompt("Enter Chunk Size.");
-alert(stringChop(str, size));
+alert(stringChop(str, size))
